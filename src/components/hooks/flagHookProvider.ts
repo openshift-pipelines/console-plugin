999b3c6a656b5ee0d5115ff7238cc242527fa407
Vikram Raj <vikrmrj@gmail.com>
2023-10-10 23:49:13 +0530

    add flag to check if TektonResult is installed

--- src/components/hooks/flagHookProvider.ts
+++ src/components/hooks/flagHookProvider.ts
@@ -1,9 +1,39 @@
-import { SetFeatureFlag } from '@openshift-console/dynamic-plugin-sdk';
-import { FLAG_HIDE_STATIC_PIPELINE_PLUGIN_PIPELINE_DETAIL_METRICS_TAB } from '../../consts';
+import * as React from 'react';
+import {
+  K8sResourceCommon,
+  SetFeatureFlag,
+  k8sGet,
+} from '@openshift-console/dynamic-plugin-sdk';
+import {
+  FLAG_HIDE_STATIC_PIPELINE_PLUGIN_PIPELINE_DETAIL_METRICS_TAB,
+  FLAG_PIPELINE_TEKTON_RESULT_INSTALLED,
+} from '../../consts';
+import { TektonResultModel } from '../../models';
 
 export const useFlagHookProvider = (setFeatureFlag: SetFeatureFlag) => {
   setFeatureFlag(
     FLAG_HIDE_STATIC_PIPELINE_PLUGIN_PIPELINE_DETAIL_METRICS_TAB,
     true,
   );
+};
+
+export const useTektonResultInstallProvider = (
+  setFeatureFlag: SetFeatureFlag,
+) => {
+  const [data, setData] = React.useState<K8sResourceCommon>();
+  React.useEffect(() => {
+    const fetch = async () => {
+      try {
+        const resource = await k8sGet({
+          model: TektonResultModel,
+          name: 'result',
+        });
+        setData(resource);
+      } catch (error) {
+        console.log('Error: ', error);
+      }
+    };
+    fetch();
+  }, []);
+  setFeatureFlag(FLAG_PIPELINE_TEKTON_RESULT_INSTALLED, data ? true : false);
 };
--- src/models.ts
+++ src/models.ts
@@ -1,39 +1,57 @@
 import { chart_color_green_400 as tektonGroupColor } from '@patternfly/react-tokens/dist/js/chart_color_green_400';
+import { K8sKind } from '@openshift-console/dynamic-plugin-sdk';
 
 const color = tektonGroupColor.value;
 
-  export const PipelineModel = {
-    apiGroup: 'tekton.dev',
-    apiVersion: 'v1',
-    label: 'Pipeline',
-    // t('pipelines-plugin~Pipeline')
-    labelKey: 'pipelines-plugin~Pipeline',
-    // t('pipelines-plugin~Pipelines')
-    labelPluralKey: 'pipelines-plugin~Pipelines',
-    plural: 'pipelines',
-    abbr: 'PL',
-    namespaced: true,
-    kind: 'Pipeline',
-    id: 'pipeline',
-    labelPlural: 'Pipelines',
-    crd: true,
-    color,
-  };
+export const PipelineModel = {
+  apiGroup: 'tekton.dev',
+  apiVersion: 'v1',
+  label: 'Pipeline',
+  // t('Pipeline')
+  labelKey: 'pipelines-plugin~Pipeline',
+  // t('Pipelines')
+  labelPluralKey: 'pipelines-plugin~Pipelines',
+  plural: 'pipelines',
+  abbr: 'PL',
+  namespaced: true,
+  kind: 'Pipeline',
+  id: 'pipeline',
+  labelPlural: 'Pipelines',
+  crd: true,
+  color,
+};
 
-  export const RepositoryModel = {
-    apiGroup: 'pipelinesascode.tekton.dev',
-    apiVersion: 'v1alpha1',
-    label: 'Repository',
-    // t('plugin__pipeline-console-plugin~Repository')
-    labelKey: 'plugin__pipeline-console-plugin~Repository',
-    // t('plugin__pipeline-console-plugin~Repositories')
-    labelPluralKey: 'plugin__pipeline-console-plugin~Repositories',
-    plural: 'repositories',
-    abbr: 'R',
-    namespaced: true,
-    kind: 'Repository',
-    id: 'repository',
-    labelPlural: 'Repositories',
-    crd: true,
-    color,
-  };
+export const RepositoryModel = {
+  apiGroup: 'pipelinesascode.tekton.dev',
+  apiVersion: 'v1alpha1',
+  label: 'Repository',
+  // t('Repository')
+  labelKey: 'plugin__pipeline-console-plugin~Repository',
+  // t('Repositories')
+  labelPluralKey: 'plugin__pipeline-console-plugin~Repositories',
+  plural: 'repositories',
+  abbr: 'R',
+  namespaced: true,
+  kind: 'Repository',
+  id: 'repository',
+  labelPlural: 'Repositories',
+  crd: true,
+  color,
+};
+
+export const TektonResultModel: K8sKind = {
+  apiGroup: 'operator.tekton.dev',
+  apiVersion: 'v1alpha1',
+  kind: 'TektonResult',
+  plural: 'tektonresults',
+  label: 'tektonresult',
+  // t('TektonResult')
+  labelKey: 'TektonResult',
+  labelPlural: 'TektonResults',
+  // t('TektonResults')
+  labelPluralKey: 'TektonResults',
+  id: 'tektonResult',
+  abbr: 'TR',
+  crd: true,
+  color: '#38812f',
+};

29c02bd687df405c744a3617fb088b0673a10615
Vikram Raj <vikrmrj@gmail.com>
2024-03-27 13:41:53 +0530

    add TriggerTemplates details

--- src/components/triggers-details/index.ts
+++ src/components/triggers-details/index.ts
@@ -1,2 +1,4 @@
 export { default as EventListenerTriggers } from './EventListenerTriggers';
 export { default as EventListenerURL } from './EventListenerURL';
+export { default as TriggerTemplateEventListeners } from './TriggerTemplateEventListeners';
+export { default as TriggerTemplatePipelines } from './TriggerTemplatePipelines';

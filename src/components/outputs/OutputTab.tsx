c0b8d7f41dd88b6f769f84961fddf8bff5361530
Vikram Raj <vikrmrj@gmail.com>
2024-04-11 20:27:11 +0530

    Add Output tab on PipelineRun details page

---
+++ src/components/outputs/OutputTab.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import {
+  EmptyState,
+  EmptyStateBody,
+  EmptyStateVariant,
+} from '@patternfly/react-core';
+import { useTranslation } from 'react-i18next';
+import { PipelineRunModel } from '../../models';
+import { PipelineRunKind } from '../../types';
+
+import ResultsList from './ResultsList';
+import { pipelineRunFilterReducer } from '../utils/pipeline-filter-reducer';
+
+const OutputTab: React.FC<{ obj: PipelineRunKind }> = ({
+  obj: pipelineRun,
+}) => {
+  const { t } = useTranslation('plugin__pipelines-console-plugin');
+
+  return pipelineRun.status?.pipelineResults || pipelineRun.status?.results ? (
+    <div className="co-m-pane__body">
+      <ResultsList
+        results={
+          pipelineRun.status?.pipelineResults || pipelineRun.status?.results
+        }
+        resourceName={t(PipelineRunModel.labelKey)}
+        status={pipelineRunFilterReducer(pipelineRun)}
+      />
+    </div>
+  ) : (
+    <EmptyState variant={EmptyStateVariant.full}>
+      <EmptyStateBody>
+        <p>{t('No Output found')}</p>
+      </EmptyStateBody>
+    </EmptyState>
+  );
+};
+export default OutputTab;

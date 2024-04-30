53d77723bb2fe550be94224e8f39662767921e06
Vikram Raj <vikrmrj@gmail.com>
2024-04-30 17:13:13 +0530

    add action options to Pipeline details page

--- src/components/pipelines-details/PipelineDetailsPage.tsx
+++ src/components/pipelines-details/PipelineDetailsPage.tsx
@@ -1,7 +1,7 @@
 import { BreadcrumbItem, Text, TextVariants } from '@patternfly/react-core';
 import { Link, useHistory } from 'react-router-dom';
 import * as React from 'react';
-import { useParams } from 'react-router-dom-v5-compat';
+import { useNavigate, useParams } from 'react-router-dom-v5-compat';
 import { useTranslation } from 'react-i18next';
 import {
   getGroupVersionKindForModel,
@@ -18,28 +18,39 @@ import DetailsPage from '../details-page/DetailsPage';
 import { navFactory } from '../utils/horizontal-nav';
 import ResourceYAMLEditorTab from '../yaml-editor/ResourceYAMLEditorTab';
 import PipelineDetails from './PipelineDetails';
-import { PipelineKind } from '../../types';
+import { PipelineKind, PipelineRunKind } from '../../types';
 import { getReferenceForModel } from '../pipelines-overview/utils';
 import PipelineParamatersTab from './PipelineParamatersTab';
 import { useLatestPipelineRun } from '../hooks/hooks';
-import { rerunPipelineAndRedirect } from '../utils/pipelines-actions';
+import { rerunPipeline } from '../utils/pipelines-actions';
+import { resourcePathFromModel } from '../utils/pipelines-utils';
+import _ from 'lodash';
+import {
+  AddTriggerModal,
+  RemoveTriggerModal,
+  startPipelineModal,
+} from '../start-pipeline';
+import { triggerPipeline } from '../pipelines-list/PipelineKebab';
+import { StartedByAnnotation } from '../../consts';
+import { usePipelineTriggerTemplateNames } from '../utils/triggers';
 
 const PipelineDetailsPage = () => {
   const { t } = useTranslation('plugin__pipelines-console-plugin');
   const params = useParams();
   const history = useHistory();
+  const navigate = useNavigate();
   const { name, ns: namespace } = params;
-  const [data, loaded] = useK8sWatchResource<PipelineKind>({
+  const [pipeline, loaded] = useK8sWatchResource<PipelineKind>({
     groupVersionKind: getGroupVersionKindForModel(PipelineModel),
     namespace,
     name,
   });
   const launchModal = useModal();
   const latestPipelineRun = useLatestPipelineRun(name, namespace);
-  const launchAnnotationsModal = useAnnotationsModal(data);
-  const launchLabelsModal = useLabelsModal(data);
-  const launchDeleteModal = useDeleteModal(data);
-
+  const launchAnnotationsModal = useAnnotationsModal(pipeline);
+  const launchLabelsModal = useLabelsModal(pipeline);
+  const launchDeleteModal = useDeleteModal(pipeline);
+  const templateNames = usePipelineTriggerTemplateNames(name, namespace) || [];
   const canEditResource = useAccessReview({
     group: PipelineModel.apiGroup,
     resource: PipelineModel.plural,
@@ -64,15 +75,65 @@ const PipelineDetailsPage = () => {
       )}/yaml`;
 
   const resourceTitleFunc = React.useMemo(() => {
-    return <div className="Pipeline-details-page">{data?.metadata?.name} </div>;
-  }, [data]);
+    return (
+      <div className="Pipeline-details-page">{pipeline?.metadata?.name} </div>
+    );
+  }, [pipeline]);
+
+  const handlePipelineRunSubmit = (pipelineRun: PipelineRunKind) => {
+    navigate(
+      resourcePathFromModel(
+        PipelineRunModel,
+        pipelineRun.metadata.name,
+        pipelineRun.metadata.namespace,
+      ),
+    );
+  };
+
+  const startPipeline = () => {
+    const params = _.get(pipeline, ['spec', 'params'], []);
+    const resources = _.get(pipeline, ['spec', 'resources'], []);
+    const workspaces = _.get(pipeline, ['spec', 'workspaces'], []);
+
+    if (!_.isEmpty(params) || !_.isEmpty(resources) || !_.isEmpty(workspaces)) {
+      launchModal(startPipelineModal, {
+        pipeline,
+        onSubmit: handlePipelineRunSubmit,
+      });
+    } else {
+      triggerPipeline(pipeline, handlePipelineRunSubmit);
+    }
+  };
+
+  const rerunPipelineAndRedirect = () => {
+    rerunPipeline(PipelineRunModel, latestPipelineRun, launchModal, {
+      onComplete: handlePipelineRunSubmit,
+    });
+  };
+
+  const addTrigger = () => {
+    const cleanPipeline: PipelineKind = {
+      ...pipeline,
+      metadata: {
+        ...pipeline.metadata,
+        annotations: _.omit(pipeline.metadata.annotations, [
+          StartedByAnnotation.user,
+        ]),
+      },
+    };
+    launchModal(AddTriggerModal, { pipeline: cleanPipeline });
+  };
+
+  const removeTrigger = () => {
+    launchModal(RemoveTriggerModal, { pipeline });
+  };
 
   if (!loaded) {
     return <LoadingBox />;
   }
   return (
     <DetailsPage
-      obj={data}
+      obj={pipeline}
       headTitle={name}
       title={<Text component={TextVariants.h1}>{resourceTitleFunc}</Text>}
       model={PipelineModel}
@@ -102,16 +163,34 @@ const PipelineDetailsPage = () => {
       ]}
       actions={[
         {
-          key: 'start-last-run',
-          label: latestPipelineRun ? t('Start last run') : null,
-          onClick: () =>
-            rerunPipelineAndRedirect(
-              PipelineRunModel,
-              latestPipelineRun,
-              launchModal,
-            ),
-          disabled: !canEditResource[0],
+          key: 'start-pipeline',
+          label: t('Start'),
+          onClick: startPipeline,
         },
+        ...(latestPipelineRun
+          ? [
+              {
+                key: 'start-last-run',
+                label: t('Start last run'),
+                onClick: rerunPipelineAndRedirect,
+                disabled: !canEditResource[0],
+              },
+            ]
+          : []),
+        {
+          key: 'add-trigger',
+          label: t('Add Trigger'),
+          onClick: addTrigger,
+        },
+        ...(templateNames.length > 0
+          ? [
+              {
+                key: 'remove-trigger',
+                label: t('Remove Trigger'),
+                onClick: removeTrigger,
+              },
+            ]
+          : []),
         {
           key: 'edit-labels',
           label: t('Edit labels'),

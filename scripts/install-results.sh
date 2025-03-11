4947f5b255410149e92d155f569da65fd1c79ecc
Vikram Raj <vikrmrj@gmail.com>
2025-03-11 20:25:26 +0530

    Update install result script

--- scripts/install-results.sh
+++ scripts/install-results.sh
@@ -1,24 +1,7 @@
 echo "Installing Tekton Results ... " 
 # Install result
 
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -nodes -subj "/CN=tekton-results-api-service.openshift-pipelines.svc.cluster.local" -addext "subjectAltName = DNS:tekton-results-api-service.openshift-pipelines.svc.cluster.local"
-
-oc create secret tls -n openshift-pipelines tekton-results-tls --cert=cert.pem --key=key.pem
-
-oc create secret generic tekton-results-postgres --namespace=openshift-pipelines --from-literal=POSTGRES_USER=result --from-literal=POSTGRES_PASSWORD=$(openssl rand -base64 20)
-
-cat << EOF | oc apply -n openshift-pipelines -f -
-apiVersion: v1
-kind: PersistentVolumeClaim
-metadata:
-  name: tekton-logs
-spec:
-  accessModes:
-  - ReadWriteOnce
-  resources:
-    requests:
-      storage: 1Gi
-EOF
+oc create secret generic tekton-results-postgres   --namespace=openshift-pipelines   --from-literal=POSTGRES_USER=result   --from-literal=POSTGRES_PASSWORD=$(openssl rand -base64 20)
 
 cat << EOF | oc apply -n openshift-pipelines -f -
 apiVersion: operator.tekton.dev/v1alpha1
@@ -27,19 +10,9 @@ metadata:
   name: result
 spec:
   targetNamespace: openshift-pipelines
-  logging_pvc_name: tekton-logs
-  logs_api: true
-  log_level: debug
-  db_port: 5432
-  db_host: tekton-results-postgres-service.openshift-pipelines.svc.cluster.local
-  logs_path: /logs
-  logs_type: File
-  logs_buffer_size: 32768
+  loki_stack_name: logging-loki
+  loki_stack_namespace: openshift-logging
   auth_disable: true
-  tls_hostname_override: tekton-results-api-service.openshift-pipelines.svc.cluster.local
-  db_enable_auto_migration: true
-  server_port: 8080
-  prometheus_port: 9090
 EOF
 
 oc create route -n openshift-pipelines passthrough tekton-results-api-service --service=tekton-results-api-service --port=8080 

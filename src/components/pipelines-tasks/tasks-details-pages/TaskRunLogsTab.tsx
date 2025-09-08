9f5bf22c3318a6a23cf131be50cecfed24d3f748
Anwesha Palit <apalit@redhat.com>
2025-09-08 16:49:44 +0530

    feat: added step url navigation in pipeline run log tab

--- src/components/pipelines-tasks/tasks-details-pages/TaskRunLogsTab.tsx
+++ src/components/pipelines-tasks/tasks-details-pages/TaskRunLogsTab.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useLocation } from 'react-router-dom-v5-compat';
 import { taskRunStatus } from '../../utils/pipeline-utils';
 import TaskRunLogs from './TaskRunLogs';
 import { TaskRunKind } from '../../../types';
@@ -9,8 +10,13 @@ export interface TaskRunDetailsProps {
 
 const TaskRunLogsTab: React.FC<TaskRunDetailsProps> = ({ obj: taskRun }) => {
   const status = taskRunStatus(taskRun);
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const activeStep = params?.get('step');
 
-  return <TaskRunLogs taskRun={taskRun} status={status} />;
+  return (
+    <TaskRunLogs taskRun={taskRun} status={status} activeStep={activeStep} />
+  );
 };
 
 export default TaskRunLogsTab;

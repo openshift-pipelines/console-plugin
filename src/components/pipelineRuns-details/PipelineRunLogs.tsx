771c63ccb62702d13df2f627e72adc4f944b81f9
Vikram Raj <vikrmrj@gmail.com>
2025-11-07 13:10:56 +0530

    Revert "fix: correctly select finally task tab via URL"
    This reverts commit 9468a42c317705e3c1dd63541103f4fe6f7f60d5.

--- src/components/pipelineRuns-details/PipelineRunLogs.tsx
+++ src/components/pipelineRuns-details/PipelineRunLogs.tsx
@@ -77,11 +77,7 @@ class PipelineRunLogsWithTranslation extends React.Component<
 
   getActiveTaskRun = (taskRuns: TaskRunKind[], activeTask: string): string => {
     const activeTaskRun = activeTask
-      ? taskRuns.find(
-          (taskRun) =>
-            taskRun.metadata?.labels?.[TektonResourceLabel.pipelineTask] ===
-            activeTask,
-        )
+      ? taskRuns.find((taskRun) => taskRun.metadata.name.includes(activeTask))
       : taskRuns.find(
           (taskRun) => taskRunStatus(taskRun) === ComputedStatus.Failed,
         ) || taskRuns[taskRuns.length - 1];
